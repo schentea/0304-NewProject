ad7438031ee01f1e332062065c474a6c23246f4a
schentea <schentea@gmail.com>
2024-03-05 16:50:46 +0900

    asd

--- js/Header_script.js
+++ js/Header_script.js
@@ -1,10 +1,10 @@
-const xmark = document.getElementById('xmark');
-const toggle = document.getElementById('toggle');
-const viewNav = document.getElementById('view_nav');
-toggle.addEventListener('click', function () {
-    viewNav.style.right = '0'; // 수정된 부분: 오른쪽으로 이동하지 않도록 값 변경
+const xmark = document.getElementById("xmark");
+const toggle = document.getElementById("toggle");
+const viewNav = document.getElementById("view_nav");
+toggle.addEventListener("click", function () {
+  viewNav.style.right = "0"; // 수정된 부분: 오른쪽으로 이동하지 않도록 값 변경
 });
 
-xmark.addEventListener('click', function () {
-    viewNav.style.right = '-100%'; // 수정된 부분: 헤더 바깥으로 나가도록 값 변경
+xmark.addEventListener("click", function () {
+  viewNav.style.right = "-100%"; // 수정된 부분: 헤더 바깥으로 나가도록 값 변경
 });
