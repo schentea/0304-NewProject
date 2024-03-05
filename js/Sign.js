a2a3ffd1d0a744d0ec20f8ee4ea066fd9c8ada73
schentea <schentea@gmail.com>
2024-03-05 16:49:03 +0900

    signin

--- js/Sign.js
+++ js/Sign.js
@@ -30,7 +30,23 @@ document
   .addEventListener("submit", function (event) {
     event.preventDefault(); // 폼 제출 기본 동작 막기 (페이지 새로고침)
     var formData = new FormData(this);
-    // 여기서부터는 회원가입 데이터를 서버로 보내는 등의 작업을 수행할 수 있습니다.
-    console.log("회원가입 정보:", formData);
-    modal.style.display = "none"; // 모달 닫기
+
+    // 여기서부터는 회원가입 데이터를 서버로 보내는 등의 작업을 수행합니다.
+    fetch("http://localhost/test/sign.php", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("회원가입이 완료되었습니다.");
+          modal.style.display = "none"; // 모달 닫기
+        } else {
+          console.error("회원가입에 실패했습니다.");
+          // 실패 시 사용자에게 알리는 코드 추가
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // 오류 시 사용자에게 알리는 코드 추가
+      });
   });
--- sign.php
+++ sign.php
@@ -6,12 +6,12 @@ header("Access-Control-Allow-Headers: Content-Type");
 $servername = "localhost"; // MySQL 호스트 이름
 $username = "root"; // MySQL 사용자 이름
 $password = "1220"; // MySQL 비밀번호
-$dbname = "test"; // 사용할 데이터베이스 이름
+$dbname = "sign"; // 사용할 데이터베이스 이름
 
 // POST로부터 받은 사용자 입력값
-$name = $_POST['name'];
-$email = $_POST['email'];
+$username2 = $_POST['username'];
 $password2 = $_POST['password'];
+$name = $_POST['name'];
 
 // 데이터베이스 연결 생성
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -22,13 +22,13 @@ if ($conn->connect_error) {
 }
 
 // 매개변수화된 SQL 쿼리 생성
-$sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+$sql = "INSERT INTO users (username, password, name) VALUES (?, ?, ?)";
 
 // SQL 쿼리를 준비합니다.
 $stmt = $conn->prepare($sql);
 
 // 매개변수에 사용자 입력값을 바인딩합니다.
-$stmt->bind_param("sss", $name, $email, $password2);
+$stmt->bind_param("sss", $username2, $password2, $name);
 
 // SQL 쿼리를 실행합니다.
 if ($stmt->execute()) {
