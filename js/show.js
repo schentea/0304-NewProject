841557152b7a6f9b373800d62e7efe7e4d85c083
LDH <ai04120@naver.com>
2024-03-08 14:03:06 +0900

    1

--- js/show.js
+++ js/show.js
@@ -1,3 +1,51 @@
+let showModal1 = document.querySelector("#showModal1");
+let showModal2 = document.querySelector("#showModal2");
+let showModal3 = document.querySelector("#showModal3");
+
+// 콘서트하우스 모달 열기
 document.querySelector("#show_right_img_div").onclick = () => {
-  document.querySelector("#showModal1").style.display = "block";
+  showModal1.style.display = "flex";
+  document.querySelector("#showModalIn1").classList.add("modalAni");
 };
+// X 모달 닫기
+document.querySelector("#showModalClose1").onclick = () => {
+  showModal1.style.display = "none";
+};
+// 모달 외부 클릭시 닫기
+window.addEventListener("click", function (event) {
+  if (event.target == showModal1) {
+    showModal1.style.display = "none";
+  }
+});
+
+// 라이크 모달 열기
+document.querySelector("#show_left2").onclick = () => {
+  showModal2.style.display = "flex";
+  document.querySelector("#showModalIn2").classList.add("modalAni");
+};
+// X 모달 닫기
+document.querySelector("#showModalClose2").onclick = () => {
+  showModal2.style.display = "none";
+};
+// 모달 외부 클릭시 닫기
+window.addEventListener("click", function (event) {
+  if (event.target == showModal2) {
+    showModal2.style.display = "none";
+  }
+});
+
+// 아트 모달 열기
+document.querySelector("#show_left1").onclick = () => {
+  showModal3.style.display = "flex";
+  document.querySelector("#showModalIn3").classList.add("modalAni");
+};
+// X 모달 닫기
+document.querySelector("#showModalClose3").onclick = () => {
+  showModal3.style.display = "none";
+};
+// 모달 외부 클릭시 닫기
+window.addEventListener("click", function (event) {
+  if (event.target == showModal3) {
+    showModal3.style.display = "none";
+  }
+});
