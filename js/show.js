5460a13fbe40f58c66dc1f610aeb5f3eaf4ce3cc
LDH <ai04120@naver.com>
2024-03-11 16:08:30 +0900

    1

--- js/show.js
+++ js/show.js
@@ -85,8 +85,8 @@ fetchData()
       )
       .join("");
     document.querySelector("#showModalList2").innerHTML = eve;
-
-    let cons = performData.filter((a) => a.place.includes("콘서트하우스"));
+    let today = new Date().toISOString().split("T")[0];
+    let cons = performData.filter((a) => a.place.includes("콘서트하우스") && a.end_date >= today);
     cons = cons
       .map(
         (a) => `<li>
