bd9e1e73e9cadcb9c8e0afc56aa846328e318e10
LDH <ai04120@naver.com>
2024-03-11 15:06:37 +0900

    1

---
+++ js/wheelAni.js
@@ -0,0 +1,15 @@
+document.addEventListener("scroll", () => {
+  for (let p of document.querySelectorAll(".scroHidden")) {
+    if (p.getBoundingClientRect().top - window.innerHeight + 50 <= 0) {
+      p.classList.add("scroVisible");
+    } else {
+      p.classList.remove("scroVisible");
+    }
+  }
+
+  for (let p of document.querySelectorAll(".blurImg")) {
+    if (p.getBoundingClientRect().top - window.innerHeight + 200 <= 0) {
+      p.classList.remove("scroBlur");
+    } else p.classList.add("scroBlur");
+  }
+});
