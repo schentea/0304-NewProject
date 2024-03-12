cbc1c8ee52e23fd4c9fc518b0ceef2b54bbb1328
YUNJINMOK <aprnaporn114@naver.com>
2024-03-12 14:12:46 +0900

    img

--- js/wheelAni.js
+++ js/wheelAni.js
@@ -1,23 +1,27 @@
-document.addEventListener("scroll", () => {
-  for (let p of document.querySelectorAll(".scroHidden")) {
-    if (p.getBoundingClientRect().top - window.innerHeight + 50 <= 0) {
-      p.classList.add("scroVisible");
-    } else {
-      p.classList.remove("scroVisible");
+document.addEventListener('scroll', () => {
+    for (let p of document.querySelectorAll('.scroHidden')) {
+        if (p.getBoundingClientRect().top - window.innerHeight + 50 <= 0) {
+            p.classList.add('scroVisible');
+        } else {
+            p.classList.remove('scroVisible');
+        }
     }
-  }
 
-  for (let p of document.querySelectorAll(".blurImg")) {
-    if (p.getBoundingClientRect().top - window.innerHeight + 150 <= 0) {
-      p.classList.remove("scroBlur");
-    } else p.classList.add("scroBlur");
-  }
+    for (let p of document.querySelectorAll('.blurImg')) {
+        if (p.getBoundingClientRect().top - window.innerHeight + 150 <= 0) {
+            p.classList.remove('scroBlur');
+        } else p.classList.add('scroBlur');
+    }
 
-  if (document.querySelector("#introH3").getBoundingClientRect().top - window.innerHeight <= 0) {
-    document.querySelector("#topWrap").classList.remove("none");
-  } else document.querySelector("#topWrap").classList.add("none");
+    if (document.querySelector('#introH3').getBoundingClientRect().top - window.innerHeight <= 0) {
+        document.querySelector('#topWrap').classList.remove('none');
+    } else document.querySelector('#topWrap').classList.add('none');
 });
 
-document.querySelector("#topBtn2").addEventListener("click", () => {
-  window.open("https://1330chat.visitkorea.or.kr:3000/#/ttalk_main/CHAT1330_160635739001093018/_0200_0200_LUIS", "_blank", "width=450,height=700,top=100,left=200");
+document.querySelector('#topBtn2').addEventListener('click', () => {
+    window.open(
+        'https://1330chat.visitkorea.or.kr:3000/#/ttalk_main/CHAT1330_160635739001093018/_0200_0200_LUIS',
+        '_blank',
+        'width=450,height=700,top=100,left=200'
+    );
 });
