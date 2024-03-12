678fa480cd21a353e87f8198767307740f2d1ee2
LDH <ai04120@naver.com>
2024-03-12 09:43:10 +0900

    1

--- js/wheelAni.js
+++ js/wheelAni.js
@@ -12,4 +12,12 @@ document.addEventListener("scroll", () => {
       p.classList.remove("scroBlur");
     } else p.classList.add("scroBlur");
   }
+
+  if (document.querySelector("#introH3").getBoundingClientRect().top - window.innerHeight <= 0) {
+    document.querySelector("#topWrap").classList.remove("none");
+  } else document.querySelector("#topWrap").classList.add("none");
+});
+
+document.querySelector("#topBtn2").addEventListener("click", () => {
+  window.open("https://1330chat.visitkorea.or.kr:3000/#/ttalk_main/CHAT1330_160635739001093018/_0200_0200_LUIS", "_blank", "width=450,height=700,top=100,left=200");
 });
