ec8cc92e8def4c6755b1b236adecf38654d57375
schentea <schentea@gmail.com>
2024-03-14 15:04:47 +0900

    ㅁㄴㅇ

--- js/Sign.js
+++ js/Sign.js
@@ -21,6 +21,7 @@ function isValidEmail(email) {
 function isValidPhoneNumber(tel) {
   return /^010\d{8}$/.test(tel);
 }
+
 function checkValidity() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
@@ -45,7 +46,7 @@ function checkValidity() {
     ? ""
     : "올바른 전화번호 형식을 입력해주세요. (예시: 01012345678)";
 
-  let submitButton = document.getElementById("submitButton");
+  let submitButton = document.getElementById("signSubmitBtn");
   submitButton.disabled = !(
     isUsernameValid &&
     isPasswordValid &&
@@ -54,49 +55,58 @@ function checkValidity() {
   );
 }
 
+function signup() {
+  let username = document.getElementById("username").value;
+
+  // 중복 아이디 확인을 위해 서버에 요청
+  fetch("http://dongseong.dothome.co.kr/check_username.php", {
+    method: "POST",
+    body: JSON.stringify({ username: username }), // 사용자 이름을 서버에 전달
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      if (data === "이미 사용 중인 아이디입니다.") {
+        document.getElementById("usernameError").textContent = data;
+      } else {
+        // 중복 아이디가 아니면 회원가입 요청 보내기
+        let formData = new FormData(document.getElementById("signupForm"));
+        fetch("http://dongseong.dothome.co.kr/sign.php", {
+          method: "POST",
+          body: formData,
+        })
+          .then((response) => {
+            if (response.ok) {
+              console.log("회원가입이 완료되었습니다.");
+              alert("회원가입이 완료되었습니다.");
+              window.location.href = "http://dongseong.dothome.co.kr/";
+            } else {
+              alert("올바른 양식을 지켜주세요.");
+              console.error("회원가입에 실패했습니다.");
+            }
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
 document
   .getElementById("signupForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
-    let username = document.getElementById("username").value;
-
-    // 중복 아이디 확인을 위해 서버에 요청
-    fetch("http://dongseong.dothome.co.kr/check_username.php", {
-      method: "POST",
-      body: JSON.stringify({ username: username }), // 사용자 이름을 서버에 전달
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        if (data === "이미 사용 중인 아이디입니다.") {
-          document.getElementById("usernameError").textContent = data;
-        } else {
-          // 중복 아이디가 아니면 회원가입 요청 보내기
-          let formData = new FormData(this);
-          fetch("http://dongseong.dothome.co.kr/sign.php", {
-            method: "POST",
-            body: formData,
-          })
-            .then((response) => {
-              if (response.ok) {
-                console.log("회원가입이 완료되었습니다.");
-                alert("회원가입이 완료되었습니다.");
-                window.location.href = "http://dongseong.dothome.co.kr/";
-              } else {
-                console.error("회원가입에 실패했습니다.");
-              }
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+    checkValidity();
+    // 클라이언트 측에서 유효성 검사를 통과한 경우에만 회원가입 요청을 보냄
+    let submitButton = document.getElementById("signSubmitBtn");
+    if (!submitButton.disabled) {
+      signup();
+    }
   });
 
 document.getElementById("username").addEventListener("input", checkValidity);
